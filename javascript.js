4dc503610aa35a8f1cd43db253199c78dddfd80f
evelyntorress <100176695+evelyntorress@users.noreply.github.com>
2022-05-08 20:10:04 -0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,28 @@
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px";
+
+});
+
+
+document.getElementById("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.color = "blue";
+
+});
+
+
+document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.opacity = "0.75";
+
+});
+
+
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px";
+
+});
+
+
